fc34f0d38a4107434e0eba22178e5e5734b39dde
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-25 22:29:53 +0500

    Add user model with schema for name, email, password, role, isActive, and age

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters long"],
+      maxlength: [50, "Name must be less than 50 characters"]
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"]
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"]
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user" 
+    },
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    age: {
+      type: Number,
+      required: [true, "Age is required"],
+      min: [18, "Age must be at least 18"],
+      max: [100, "Age must be below 100"]
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
