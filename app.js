3aae027d18988aaf59a5facd9b95fb930f515e27
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-25 22:27:16 +0500

    Refactor app.js to implement route handling with modular routes and connect to MongoDB

--- app.js
+++ app.js
@@ -1,82 +1,26 @@
-import express from'express' ;
-import  Joi from 'joi'; //used for validation
+import express from "express";
+import dotenv from "dotenv";
+import { connectdb } from "./config/Database.js";
+import bookRoutes from "./routes/books.route.js";
+import userRoutes from "./routes/user.route.js"; 
+import authRoutes from "./routes/auth.route.js";
+
+
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectdb();
+
+// Middleware
 app.use(express.json());
- 
-const books = [
-{title: 'Harry Potter', id: 1},
-{title: 'Twilight', id: 2},
-{title: 'Lorien Legacies', id: 3}
-]
+
+// Routes
+app.use("/api/books", bookRoutes); 
+app.use("/api/auth", authRoutes); 
+app.use("/api/users", userRoutes); 
 
 
-//READ Request Handlers
-app.get('/', (req, res) => {
-res.send('Welcome to Edurekas REST API with Node.js Tutorial!!');
-});
- 
-app.get('/api/books', (req,res)=> {
-res.send(books);
-});
- 
-app.get('/api/books/:id', (req, res) => {
-const book = books.find(c => c.id === parseInt(req.params.id));
- 
-if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
-res.send(book);
-});
- 
-//CREATE Request Handler
-app.post('/api/books', (req, res)=> {
- 
-const { error } = validateBook(req.body);
-if (error){
-res.status(400).send(error.details[0].message)
-return;
-}
-const book = {
-id: books.length + 1,
-title: req.body.title
-};
-books.push(book);
-res.send(book);
-});
- 
-//UPDATE Request Handler
-app.put('/api/books/:id', (req, res) => {
-const book = books.find(c=> c.id === parseInt(req.params.id));
-if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Not Found!! </h2>');
- 
-const { error } = validateBook(req.body);
-if (error){
-res.status(400).send(error.details[0].message);
-return;
-}
- 
-book.title = req.body.title;
-res.send(book);
-});
- 
-//DELETE Request Handler
-app.delete('/api/books/:id', (req, res) => {
- 
-const book = books.find( c=> c.id === parseInt(req.params.id));
-if(!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
- 
-const index = books.indexOf(book);
-books.splice(index,1);
- 
-res.send(book);
-});
- 
-function validateBook(book) {
-const schema = {
-title: Joi.string().min(3).required()
-};
-return Joi.validate(book, schema);
- 
-}
- 
-//PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
