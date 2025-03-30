9bd32887ec5066ee65e940e24d8bfeec558383a5
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2025-03-30 23:40:48 +0500

    Update generateToken.util.js

--- utils/generateToken.util.js
+++ utils/generateToken.util.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (userId, role) => {
+const accessToken = (userId, role) => {
   return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
