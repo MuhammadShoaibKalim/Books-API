59aff75f5c8352a6e4b9175fa631c20229d26714
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2025-03-31 16:38:31 +0500

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin", "superadmin"],
+      enum: ["user", "admin"],
       default: "user" 
     },
     isActive: {
